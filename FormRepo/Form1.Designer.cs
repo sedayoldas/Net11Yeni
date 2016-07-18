818257f13baffb1b548c58a38ba123d06a4f352a
sedayoldas <sedaylds@windowslive.com>
2016-07-18 17:13:46 +0300

    new change

---
+++ FormRepo/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FormRepo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.gvProducts = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(83, 47);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(166, 20);
+            this.txtMin.TabIndex = 0;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(86, 79);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(166, 20);
+            this.txtMax.TabIndex = 1;
+            // 
+            // gvProducts
+            // 
+            this.gvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvProducts.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.gvProducts.Location = new System.Drawing.Point(0, 162);
+            this.gvProducts.Name = "gvProducts";
+            this.gvProducts.Size = new System.Drawing.Size(501, 195);
+            this.gvProducts.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MIN:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MAX:";
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(276, 79);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 4;
+            this.btnAra.Text = "ARA";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 357);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gvProducts);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.DataGridView gvProducts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAra;
+    }
+}
---
+++ FormRepo/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormRepo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        { 
+            NWDataContext db = new NWDataContext();
+
+            var sorgu1 = from urun in db.Products
+                         where urun.UnitPrice >= decimal.Parse(txtMin.Text) &&
+                         urun.UnitPrice <= decimal.Parse(txtMax.Text)
+                         orderby urun.UnitPrice ascending 
+                         select urun; /* 
+                                       select new {
+                                       urun.productName,
+                                       urun.UnitPrice,
+                                       };
+                                       */
+
+            var sorgu2 = db.Products.Where(urun => urun.UnitPrice >= decimal.Parse(txtMin.Text) &&
+                         urun.UnitPrice <= decimal.Parse(txtMax.Text))
+                         .Select(urun => new { urun.ProductName, urun.UnitPrice, urun.QuantityPerUnit }).OrderBy(u=>u.ProductName);
+
+             //var sorgu2 = db.Products.Where(urun => urun.UnitPrice >= decimal.Parse(txtMin.Text) &&
+             //            urun.UnitPrice <= decimal.Parse(txtMax.Text)).Select(u=>u);
+
+            //var sorgu2 = db.Products.Where(urun => urun.UnitPrice >= decimal.Parse(txtMin.Text) &&
+            //            urun.UnitPrice <= decimal.Parse(txtMax.Text));
+
+            gvProducts.DataSource = sorgu2;
+        }
+    }
+}
